7bce5ede75ff3678c0d54ebd261fe1deef79712e
Niclas Hammar <niclas.hammar@frontedgeit.se>
2019-01-24 14:57:48 +0100

    Add outline for necessary components

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,25 @@
 import React from "react";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+
+const PageOne = () => {
+  return <div>Page One</div>;
+};
+
+const PageTwo = () => {
+  return <div>Page two</div>;
+};
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Route path="/" exact component={PageOne} />
+          <Route path="/pagetwo" component={PageTwo} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
