72a3907042248f39444e750cbcf01d88f4623dc0
Niclas Hammar <niclas.hammar@frontedgeit.se>
2019-02-13 13:27:15 +0100

    Add action creators for signin/out

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { signIn, signOut } from "../actions";
 
-export default class GoogleAuth extends Component {
+class GoogleAuth extends Component {
   state = { isSignedIn: null };
   componentDidMount() {
     window.gapi.load("client:auth2", () => {
@@ -19,10 +21,12 @@ export default class GoogleAuth extends Component {
         });
     });
   }
-  onAuthChange = () => {
-    this.setState({
-      isSignedIn: this.auth.isSignedIn.get()
-    });
+  onAuthChange = isSignedIn => {
+    if (isSignedIn) {
+      this.props.signIn();
+    } else {
+      this.props.signOut();
+    }
   };
   onSignInClick = () => {
     this.auth.signIn();
@@ -54,3 +58,7 @@ export default class GoogleAuth extends Component {
     return <div className=" item">{this.renderAuthButton()}</div>;
   }
 }
+export default connect(
+  null,
+  { signIn, signOut }
+)(GoogleAuth);
