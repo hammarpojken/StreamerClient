600bf9b8e3bf8ca6cdcb0a67f488449d33d395c6
Niclas Hammar <niclas.hammar@frontedgeit.se>
2019-02-13 17:14:14 +0100

    Added form validation using redux form validate

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -2,11 +2,17 @@ import React, { Component } from "react";
 import { Field, reduxForm } from "redux-form";
 
 class StreamCreate extends Component {
+  //rendering the specific input fields, props that are passed down from the field componenet in redux-form
+  renderError({ error, touched }) {
+    return <div>{error}</div>;
+  }
+
   renderInput(formProps) {
     return (
-      <div>
+      <div className="field">
         <label>{formProps.label}</label>
         <input {...formProps.input} />
+        {this.renderError(formProps.meta)}
       </div>
     );
   }
@@ -24,12 +30,28 @@ class StreamCreate extends Component {
           component={this.renderInput}
           label="Enter a description"
         />
-        <button className=" ui button primary">Submit</button>
+        <button type="submit" className=" ui button primary">
+          Submit
+        </button>
       </form>
     );
   }
 }
 
+// redux-form uses this function to validate the formvalues that is submitet passed in down below
+const validate = formValues => {
+  const errors = {};
+
+  if (!formValues.title) {
+    errors.title = "You must enter som values for the titles!";
+  }
+  if (!formValues.description) {
+    errors.description = "You must enter a description!";
+  }
+  return errors;
+};
+
 export default reduxForm({
-  form: "streamCreate"
+  form: "streamCreate",
+  validate: validate
 })(StreamCreate);
