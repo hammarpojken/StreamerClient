2f2f25984cc54c15f1d0a5c4553e26c247178f3c
Niclas Hammar <niclas.hammar@frontedgeit.se>
2019-02-14 09:12:00 +0100

    Add error messages if user input is invalid

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -3,26 +3,39 @@ import { Field, reduxForm } from "redux-form";
 
 class StreamCreate extends Component {
   //rendering the specific input fields, props that are passed down from the field componenet in redux-form
-  renderError({ error, touched }) {
-    return <div>{error}</div>;
+  renderError(meta) {
+    console.log(meta);
+    if (meta.touched && meta.error) {
+      console.log("inside");
+      return (
+        <div className="ui error message">
+          <div className="header">{meta.error}</div>
+        </div>
+      );
+    }
   }
 
-  renderInput(formProps) {
+  renderInput = formProps => {
+    const className = `field ${
+      formProps.meta.touched && formProps.meta.error ? "error" : ""
+    }`;
     return (
-      <div className="field">
+      <div className={className}>
         <label>{formProps.label}</label>
         <input {...formProps.input} />
         {this.renderError(formProps.meta)}
       </div>
     );
+  };
+  onSubmit(formValues) {
+    console.log(formValues);
   }
-  onSubmit(formValues) {}
 
   render() {
     return (
       <form
         onSubmit={this.props.handleSubmit(this.onSubmit)}
-        className="ui form"
+        className="ui form error"
       >
         <Field name="title" component={this.renderInput} label="Enter a name" />
         <Field
@@ -30,9 +43,7 @@ class StreamCreate extends Component {
           component={this.renderInput}
           label="Enter a description"
         />
-        <button type="submit" className=" ui button primary">
-          Submit
-        </button>
+        <button className=" ui button primary">Submit</button>
       </form>
     );
   }
